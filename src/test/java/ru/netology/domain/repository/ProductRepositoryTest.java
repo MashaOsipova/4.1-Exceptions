28ab3bbe9867ad60cad23a385a2f6de904d04009
MashaOsipova <mashaosi395@mail.ru>
2022-02-16 11:12:47 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/repository/ProductRepositoryTest.java
@@ -0,0 +1,35 @@
+package ru.netology.domain.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.ProductRepository;
+
+class ProductRepositoryTest {
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book();
+    private Product item = new Product(4, "Product for remove", 500);
+
+    @Test
+    public void shouldRemoveByIdIfExistTest() {
+        repository.save(item);
+        repository.save(coreJava);
+        repository.removeById(4);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowNotFoundExceptionIfRemoveByIdNotExistTest() {
+        repository.save(item);
+        repository.save(coreJava);
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(15);
+        });
+    }
+}
